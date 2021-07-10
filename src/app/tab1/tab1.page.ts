7d60227c208bb969b0b4c012d5ac6115ca903e90
TXT-53 <txt-53@TXT-53.local>
2021-07-10 12:12:54 -0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component ,ElementRef,ViewChild} from '@angular/core';
+import { Gesture, GestureController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+ 
+ 
+  constructor(private gestureCtrl: GestureController) {
+   this.gestureCtrl = gestureCtrl;
+  }
+  @ViewChild('swipeContainer') swipeContainer: ElementRef;
+  ngAfterViewChecked(){ 
+    const gesture: Gesture = this.gestureCtrl.create({
+      el: this.swipeContainer.nativeElement,
+      threshold: 15,
+      gestureName: 'my-gesture',
+      onMove: detail => this.onMove(detail)
+    }, true);
+    // The `true` above ensures that callbacks run inside NgZone.
+    gesture.enable()
 
-  constructor() {}
-
+  }
+  
+  
+  private onMove(detail) {
+    const type = detail.type;
+    const currentX = detail.currentX;
+    const deltaX = detail.deltaX;
+    const velocityX = detail.velocityX;
+    console.log(type);
+    console.log(currentX);
+    console.log(deltaX);
+    console.log(velocityX);
+  }
+ 
 }
