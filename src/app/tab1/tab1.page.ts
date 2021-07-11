a6b4187fc2e9ae36494af5a52151b5b87ea939f6
TXT-53 <txt-53@TXT-53.local>
2021-07-10 18:27:24 -0700

    this is the code for the swipe

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component ,ElementRef,ViewChild} from '@angular/core';
 import { Gesture, GestureController } from '@ionic/angular';
+import { ThrowStmt } from '@angular/compiler';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,10 +8,39 @@ import { Gesture, GestureController } from '@ionic/angular';
 })
 
 export class Tab1Page {
- 
- 
+  usersIndex:number;
+  users:any;
+  currentChoice:any;
+  readSwipe: boolean;
+  rightPeople: any = [];
+
+
   constructor(private gestureCtrl: GestureController) {
-   this.gestureCtrl = gestureCtrl;
+    this.readSwipe = true;
+    this.gestureCtrl = gestureCtrl;
+   this.users = [
+     {
+       name: "Issabella",
+       sign: "Taurus",
+       age: 19,
+       city: "Los Angeles"
+     },
+     {
+       name : "Yeshuwa",
+       sign : "leo",
+       age : 18,
+       city: "Arkansas",
+
+     },
+     {
+       name : "Jacky",
+       sign : "Pisces",
+      age: 45,
+      city: "Fullerton"
+     },
+   ]
+   this.usersIndex = 0;
+   this.currentChoice = this.users[this.usersIndex];
   }
   @ViewChild('swipeContainer') swipeContainer: ElementRef;
   ngAfterViewChecked(){ 
@@ -18,26 +48,45 @@ export class Tab1Page {
       el: this.swipeContainer.nativeElement,
       threshold: 15,
       gestureName: 'my-gesture',
-      onMove: detail => this.onMove(detail)
+      onMove: detail => this.onMove(detail),
+      onEnd: detail => this.onEnd(detail)
     }, true);
     // The `true` above ensures that callbacks run inside NgZone.
     gesture.enable()
 
   }
   
-  
+  // swipe
   private onMove(detail) {
     const type = detail.type;
     const currentX = detail.currentX;
     const deltaX = detail.deltaX;
     const velocityX = detail.velocityX;
-  
-    if(velocityX > 0){
+   
+    //right
+    if(velocityX > 1.4 && this.readSwipe){
+      this.usersIndex = this.usersIndex + 1;
+      this.currentChoice = this.users[this.usersIndex]
       console.log("Swiped Right")
+      console.log(this.currentChoice)
+      this.readSwipe = false;
+      this.rightPeople.push(this.currentChoice)
+      console.log(this.rightPeople)
     }
-    else if(velocityX < 0){
+    //left
+    else if(velocityX < -1.4 && this.readSwipe){
+      
+      this.usersIndex = this.usersIndex + 1;
+      this.currentChoice = this.users[this.usersIndex]
+      console.log(this.currentChoice)
+      this.readSwipe = false;
       console.log("Swiped Left")
+      
     }
   }
+  private onEnd(detail){
+    this.readSwipe = true;
+  }
+  
  
 }
